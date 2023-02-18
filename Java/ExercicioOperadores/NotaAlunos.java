6f259703015bf7cdfaad22f85cd097d9ad74fff7
DRCreatorZ <dener.r.c@hotmail.com>
2023-02-17 22:01:22 -0300

    aula 17/02/23

---
+++ Java/ExercicioOperadores/NotaAlunos.java
@@ -0,0 +1,17 @@
+package ExercicioOperadores;
+
+public class NotaAlunos {
+    public void aprovacaoAluno() {
+        System.out.println("===============================");
+        int primeiraNota = 100;
+        int segundaNota = 50;
+        int frequencia = 75;
+        System.out.println("O valor da primeira Nota é "
+                + primeiraNota + " e o da segunda é "
+                + segundaNota + ".");
+        int media = primeiraNota+segundaNota/2;
+        System.out.println("A media do Aluno é "+ media );
+        System.out.println("A frequencia do Aluno é "+ frequencia );
+        
+            }
+}
