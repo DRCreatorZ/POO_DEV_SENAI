4d1f631181feb38d1d2f0eed37f56c8116c176fb
DRCreatorZ <dener.r.c@hotmail.com>
2023-02-24 22:01:24 -0300

    aula 24/02/23

--- Java/ExercicioOperadores/App.java
+++ Java/ExercicioOperadores/App.java
@@ -2,6 +2,7 @@ package ExercicioOperadores;
 
 public class App {
     public static void main(String[] args) {
-        
+        NotaAlunos obj1 = new NotaAlunos();
+        obj1.aprovacaoAluno();
     }
 }
--- Java/ExercicioOperadores/NotaAlunos.java
+++ Java/ExercicioOperadores/NotaAlunos.java
@@ -1,17 +1,37 @@
 package ExercicioOperadores;
 
+import java.util.Scanner;
+
 public class NotaAlunos {
+    Scanner sc = new Scanner(System.in);
+
     public void aprovacaoAluno() {
-        System.out.println("===============================");
-        int primeiraNota = 100;
-        int segundaNota = 50;
-        int frequencia = 75;
-        System.out.println("O valor da primeira Nota é "
-                + primeiraNota + " e o da segunda é "
-                + segundaNota + ".");
-        int media = primeiraNota+segundaNota/2;
-        System.out.println("A media do Aluno é "+ media );
-        System.out.println("A frequencia do Aluno é "+ frequencia );
-        
-            }
+        System.out.println("=========================================");
+        System.out.println("Qual o valor da Nota 1? ");
+        int Nota1 = sc.nextInt();
+        System.out.println("Qual valor da Nota 2? ");
+        int Nota2 = sc.nextInt();
+
+        System.out.println("========================================");
+        double media = (Nota1 + Nota2) / 2;
+        System.out.println("A media do Aluno é >> " + media);
+
+        System.out.println("========================================");
+        boolean mediaAprovado = media >= 50;
+        System.out.println("O aluno está aprovado por nota? >> " + mediaAprovado);
+
+        System.out.println("========================================");
+        System.out.println("Qual a frequência do Aluno? >> ");
+        int frequencia = sc.nextInt();
+
+        System.out.println("=========================================");
+        boolean frequenciaAprovado = frequencia >= 75;
+        System.out.println("O aluno está aprovado por frequência? >> " + frequenciaAprovado);
+
+        System.out.println("=========================================");
+        boolean aprovacaoFinal = mediaAprovado && frequenciaAprovado;
+        System.out.println("O aluno está aprovado no Curso? >> " + aprovacaoFinal);
+
+        System.out.println("=========================================");
+    }
 }
